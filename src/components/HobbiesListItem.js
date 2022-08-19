852e71df44bbeedce1cfbcf5415b02f5a4a589cb
Nikki Purcell <nikki.purcell@chownow.com>
2022-08-19 09:20:05 -0700

    test 2

--- src/components/HobbiesListItem.js
+++ src/components/HobbiesListItem.js
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 import "../App.css";
 
 const HobbiesListItem = ({ id, name, src }) => {
-  const imgSrc = `../images/${src}.jpg`;
+  const imgSrc = `./images/${src}.jpg`;
   const pageUrl = `/hobbies/${src}`;
 
   return (
     <li key={id}>
       <Link to={pageUrl}>
-        <img src={imgSrc} alt={name} />
+        <img src={require(imgSrc)} alt={name} />
       </Link>
     </li>
   );
