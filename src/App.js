ddd1d9d3e3fefa558c3fbb52a058c0bd46e179d9
Nikki Purcell <nikki.purcell@chownow.com>
2022-08-19 08:26:06 -0700

    Add redirect for base route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Category from "./pages/Category";
 import Hobby from "./pages/Hobby";
@@ -11,9 +11,11 @@ const App = () => {
     <div className="App">
       <Routes>
         <Route path="/hobby" element={<Home />} />
+        <Route path="/" element={ <Navigate to="/hobby" /> } />
         <Route path="/category/:category" element={<Category />} />
         <Route path="/hobbies/:hobby" element={<Hobby />} />
         <Route element={<NoMatch />} />
+
       </Routes>
     </div>
   );
