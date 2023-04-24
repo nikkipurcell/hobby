594afae6f57213cba9fb7934d751a5829dc461eb
Nikki Purcell <nikki.purcell@chownow.com>
2023-04-23 21:37:35 -0700

    Updates

---
+++ precache-manifest.2a854ae43a96f95f1efccc167f36216a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6fb38dd1ed5f3fcb798",
+    "url": "/hobby/static/css/main.58bc0f7a.chunk.css"
+  },
+  {
+    "revision": "a6fb38dd1ed5f3fcb798",
+    "url": "/hobby/static/js/main.0f625368.chunk.js"
+  },
+  {
+    "revision": "1e774ffa0235eeae4034",
+    "url": "/hobby/static/js/runtime~main.e349db77.js"
+  },
+  {
+    "revision": "6fc1fa0a490890a47e98",
+    "url": "/hobby/static/css/2.0704e4c0.chunk.css"
+  },
+  {
+    "revision": "6fc1fa0a490890a47e98",
+    "url": "/hobby/static/js/2.bffdbcae.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/hobby/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/hobby/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/hobby/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/hobby/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/hobby/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "9bad567ce0077e71fd5350dcec9bbd67",
+    "url": "/hobby/index.html"
+  }
+];
